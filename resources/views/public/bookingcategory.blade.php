d86d091ae2a4d00c3a67ae78018f0cc66418e217
rahulxdev0 <rk854327@gmail.com>
2024-11-13 23:58:44 +0530

    footer fixed

--- resources/views/public/bookingcategory.blade.php
+++ resources/views/public/bookingcategory.blade.php
@@ -1,24 +1,25 @@
 @extends('public.layout');
 @section('content')
-<div class="w-full flex justify-center items-center">
+    <div class="w-full flex justify-center items-center">
 
 
-    <div class=" bg-zinc-950 h-auto flex items-center justify-center p-6">
-        <div class="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            <!-- Card 1 -->
+        <div class=" bg-zinc-950 h-auto flex items-center justify-center p-6">
+            <div class="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                <!-- Card 1 -->
 
-            @foreach ($bookcat as $cat )
-            <a href="{{route('category.name',$cat->cat_name)}}" class="bg-zinc-900 shadow-lg rounded-lg p-6 transform hover:scale-105 transition-transform duration-300">
-                <img src="{{ asset('images/' . $cat->cat_image) }}" alt="Card Image" class="w-full h-24 object-cover rounded-md mb-4">
-                <h3 class="text-xl text-center text-white  font-semibold mb-2 capitalize">{{ $cat->cat_name }}</h3>
-            </a>
-            @endforeach
+                @foreach ($bookcat as $cat)
+                    <a href="{{ route('category.name', $cat->cat_name) }}"
+                        class="bg-zinc-900 shadow-lg rounded-lg p-6 transform hover:scale-105 transition-transform duration-300">
+                        <img src="{{ asset('images/' . $cat->cat_image) }}" alt="Card Image"
+                            class="w-full h-24 object-cover rounded-md mb-4">
+                        <h3 class="text-xl text-center text-white  font-semibold mb-2 capitalize">{{ $cat->cat_name }}</h3>
+                    </a>
+                @endforeach
 
-          
+
+            </div>
         </div>
+
+
     </div>
-
-
-</div>
-
 @endsection
--- resources/views/public/layout.blade.php
+++ resources/views/public/layout.blade.php
@@ -8,90 +8,92 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css"
         rel="stylesheet">
-        <style>
+    <style>
+        .gallery {
+            width: 85vw;
+            height: 60vmin;
+            display: flex;
+            gap: 10px;
+            margin: 0 auto;
+        }
+
+        .gallery img {
+            height: 100%;
+            flex: 1;
+            object-fit: cover;
+            overflow: hidden;
+            cursor: pointer;
+            border-radius: 10px;
+            transition: all 0.3s;
+        }
+
+        .gallery img:hover {
+            flex: 4;
+        }
+
+
+        @media (max-width: 768px) {
             .gallery {
-                width: 85vw;
-                height: 60vmin;
-                display: flex;
-                gap: 10px;
-                margin: 0 auto;
+                flex-direction: column;
+                height: auto;
             }
-    
+
             .gallery img {
-                height: 100%;
-                flex: 1;
-                object-fit: cover;
-                overflow: hidden;
-                cursor: pointer;
-                border-radius: 10px;
-                transition: all 0.3s;
+                height: auto;
+                width: 100%;
             }
-    
-            .gallery img:hover {
-                flex: 4;
-            }
-    
-    
-            @media (max-width: 768px) {
-                .gallery {
-                    flex-direction: column;
-                    height: auto;
-                }
-    
-                .gallery img {
-                    height: auto;
-                    width: 100%;
-                }
-            }
-    
+        }
+
+        .gallery {
+            width: 85vw;
+            height: 60vmin;
+            display: flex;
+            gap: 10px;
+            margin: 0 auto;
+        }
+
+        .gallery img {
+            height: 100%;
+            flex: 1;
+            object-fit: cover;
+            overflow: hidden;
+            cursor: pointer;
+            border-radius: 10px;
+            transition: all 0.3s;
+        }
+
+        .gallery img:hover {
+            flex: 4;
+        }
+
+
+        @media (max-width: 768px) {
             .gallery {
-                width: 85vw;
-                height: 60vmin;
-                display: flex;
-                gap: 10px;
-                margin: 0 auto;
+                flex-direction: column;
+                height: auto;
             }
-    
+
             .gallery img {
-                height: 100%;
-                flex: 1;
-                object-fit: cover;
-                overflow: hidden;
-                cursor: pointer;
-                border-radius: 10px;
-                transition: all 0.3s;
+                height: auto;
+                width: 100%;
             }
-    
-            .gallery img:hover {
-                flex: 4;
-            }
-    
-    
-            @media (max-width: 768px) {
-                .gallery {
-                    flex-direction: column;
-                    height: auto;
-                }
-    
-                .gallery img {
-                    height: auto;
-                    width: 100%;
-                }
-            }
-        </style>
-        @livewireStyles
+        }
+    </style>
+    @livewireStyles
 </head>
 
 <body class="bg-white min-h-screen">
     <div class="h-180 flex flex-col md:flex-row">
-       @include('public.sidebar')
+        @include('public.sidebar')
 
 
-    @section('content')
+        @section('content')
 
-    @show
-   
-    
+        @show
+
+        @include('public.footer')
+
+    </div>
 </body>
 @livewireScripts
 
