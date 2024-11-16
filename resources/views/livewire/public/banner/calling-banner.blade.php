e08be69404e46d59ef3ecd20f4e98c243b3aec68
aditi-19112002 <aditikeshri21@gmail.com>
2024-11-16 15:22:27 +0530

    work on home page

--- resources/views/livewire/public/banner/calling-banner.blade.php
+++ resources/views/livewire/public/banner/calling-banner.blade.php
@@ -1,9 +1,10 @@
 <div>
     <img d="background-image" src="{{ $backgroundImage }}"
-        alt="Fullscreen Image" class="object-cover w-full h-450px">
+        alt="Fullscreen Image" class="object-cover w-full h-450">
 
     <div class="absolute inset-0 bg-black bg-opacity-50 flex flex-col text-center p-4">
-    <div class="flex flex-col md:flex-row items-center justify-between mt-4 px-6 md:px-10">
+    
+      <div class="flex flex-col md:flex-row items-center justify-center mt-4 px-6 md:px-10">
   
     <h1 class="text-white text-3xl md:text-5xl font-bold mb-2 mr-5 ml-0 md:ml-10 text-center md:text-left">
         WELCOME TO <span class="text-yellow-400">SMART STUDIO</span>
@@ -18,10 +19,12 @@
         </button>
     </form>
 </div>
-
-<p class="text-white text-lg md:text-xl mt-2 text-center md:text-left px-4">
+<div class="flex flex-col md:flex-row items-center justify-center mt-4 px-6 md:px-10 ml-12">
+<p class="text-white text-lg md:text-xl  text-center md:text-left">
     This is a paragraph inside the fullscreen image. The content is centered both vertically and horizontally.
-</p>
+</p> 
+</div>
+
 
         <div class="absolute bottom-36 left-1/2 transform -translate-x-1/2 flex items-center space-x-6">
             {{-- <img src="https://th.bing.com/th?q=Menu+Icon+On+Black+Background&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247"
