956faf71678ba17f65193bb338533fd75dac09bd
SaritaKumari24 <saritaakumari24@gmail.com>
2024-11-16 14:43:56 +0530

    small changes in navbar in admin panel

--- resources/views/admin/includes/navbar.blade.php
+++ resources/views/admin/includes/navbar.blade.php
@@ -50,7 +50,7 @@
         </button>
     </div>
     <div class="w-full md:w-1/3 h-auto flex items-center justify-center mb-2 md:mb-0"></div>
-    <div class="w-full md:w-1/3 h-auto p-2 flex items-center justify-center">
+    {{-- <div class="w-full md:w-1/3 h-auto p-2 flex items-center justify-center">
         <div class="relative w-full">
             <input type="text" placeholder="Search..." class="w-full pl-4 pr-10 py-2 bg-white rounded-full focus:outline-none border border-yellow-400">
             <div class="absolute inset-y-0 right-0 flex items-center pr-3">
@@ -61,7 +61,7 @@
                 </button>
             </div>
         </div>
-    </div>
+    </div> --}}
     <div class="absolute right-4 top-4 w-full md:w-auto h-auto flex items-center gap-2">
         <div class="w-full md:w-24 h-10 rounded-lg flex items-center justify-center text-center">
             <form action="{{ route('logout') }}" method="POST" class="w-full">
