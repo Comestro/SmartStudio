8893af960c948a6290a795ca635271af392dc0da
Saumy Anand <samcool3203@gmail.com>
2024-11-19 16:58:08 +0530

    crud added in ad section

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -43,38 +43,48 @@ class AdController extends Controller
 
     // public function show(AdInformation $item)
     // {
-    //     return view('items.show', compact('item'));
+    //     $data['items'] = AdInformation::find($item);
+    //         dd($data['items']);
+    //       return view('admin.Ads.editAd',$data);
     // }
 
-    public function edit(AdInformation $item)
-    {
-        $data['items']=$item;
+    public function edit($item)
+{
+        $data['items'] = AdInformation::find($item);
         return view('admin.Ads.editAd',$data);
     }
 
-    public function update(Request $request, AdInformation $item)
+    public function update(Request $request,  $item)
     {
         $request->validate([
             'image' => 'nullable|image',
             'title' => 'required|string|max:255',
             'description' => 'required|string',
-            'status' => 'required|in:available,unavailable',
+            'status' => 'required',
             'price' => 'required|numeric',
         ]);
+        $items= AdInformation::find($item);
 
         if ($request->hasFile('image')) {
             $imagePath = $request->file('image')->store('images', 'public');
-            $item->update(['image' => $imagePath]);
+            $items->update(['image' => $imagePath]);
         }
 
-        $item->update($request->only('title', 'description', 'status', 'price'));
+
+        $items->update(['title'=>$request->title,
+        'description'=>$request->description,
+        'status'=>$request->status,
+        'price'=>$request->price,
+        ]
+        );
 
         return redirect()->route('managead.index')->with('success', 'Item updated successfully.');
     }
 
-    public function destroy(AdInformation $item)
+    public function destroy($item)
     {
-        $item->delete();
+        $data['items']=AdInformation::find($item);
+        $data['items']->delete();
         return redirect()->route('managead.index')->with('success', 'Item deleted successfully.');
     }
 }
