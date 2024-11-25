0fc5c358717c5575c5939658ce00c37ead0463ba
smi-riti <smriti.kumari5104@gmail.com>
2024-11-25 16:01:34 +0530

    navbar

--- resources/views/public/base.blade.php
+++ resources/views/public/base.blade.php
@@ -17,12 +17,12 @@
 <body class=" bg-black roboto-medium">
 
 
-    <header class="bg-gradient-to-b from-gray-900 to-black shadow-md text-white px-4 fixed top-0 w-full z-10 ">
-        <div class="container mx-auto flex items-center justify-between p-4">
+    <header class="bg-gradient-to-b from-gray-900 to-black shadow-md text-white px-2 lg:px-4 fixed top-0 w-full z-10 ">
+        <div class="container mx-auto flex items-center justify-between p-2 lg:p-4">
             <!-- Logo Section -->
             <div class="flex items-center space-x-3">
                <a href="{{ route('home') }}"><img src="https://th.bing.com/th?id=OIP.XfloBSB32f50RImZskOHFwHaHa&w=250&h=250" alt="Logo" class="w-12 h-12 rounded-full"></a> 
-                <a href="{{ route('home') }}" class="text-xl font-bold">Smart Studio</a>
+                <a href="{{ route('home') }}" class=" text-md lg:text-xl font-bold">Smart Studio</a>
             </div>
         
             <!-- Hamburger Menu Button (Visible on small screens) -->
@@ -35,17 +35,19 @@
             </div>
         
             <!-- Navigation Links -->
-            <nav id="menu" class="hidden md:flex md:space-x-6 font-semibold text-lg">
+            <nav id="menu" class="hidden md:flex md:space-x-4 font-semibold text-sm lg:text-lg">
                 <a href="{{ route('home') }}" class="text-yellow-500">Home</a>
                 <a href="{{ route('about') }}" class="hover:text-yellow-500">About</a>
                 <a href="{{ route('gallery') }}" class="hover:text-yellow-500">Gallery</a>
                 <a href="{{ route('portfolio') }}" class="hover:text-yellow-500">Portfolio</a>
                 <a href="{{ route('contact') }}" class="hover:text-yellow-500">Contact</a>
+                <a href="{{ route('budget.index') }}" class="hover:text-yellow-500">Budget</a>
+                
             </nav>
         
             <!-- Social Icons -->
-            <div class="hidden md:flex space-x-4">
-                <a href="https://facebook.com" target="_blank" class="hover:text-blue-600">
+            <div class="hidden md:flex space-x-2">
+                {{-- <a href="https://facebook.com" target="_blank" class="hover:text-blue-600">
                     <i class="bi bi-facebook text-xl"></i>
                 </a>
                 <a href="https://instagram.com" target="_blank" class="hover:text-pink-500">
@@ -53,19 +55,69 @@
                 </a>
                 <a href="https://twitter.com" target="_blank" class="hover:text-blue-400">
                     <i class="bi bi-twitter text-xl"></i>
+                </a> --}}
+                <a href="{{ route('category.view') }}"
+                class="block text-center text-yellow-400 border border-yellow-400 px-2 py-2 rounded hover:bg-yellow-400 hover:text-white transition">
+                Book Now
                 </a>
+
+                @auth
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit"
+                        class="w-full px-2 py-2 text-yellow-400 border border-yellow-400 rounded hover:bg-yellow-400 hover:text-white transition">
+                        Logout
+                    </button>
+                </form>
+                @endauth
+                @guest
+                <form action="{{ route('login') }}">
+                    @csrf
+                    <button type="submit"
+                        class="w-full px-2 py-2 text-yellow-400 border border-yellow-400 rounded hover:bg-yellow-400 hover:text-white transition">
+                        Login
+                    </button>
+                </form>
+                @endguest
             </div>
         </div>
         
         <!-- Mobile Menu -->
-        <div id="mobile-menu" class="hidden md:hidden flex flex-col space-y-4 p-4">
-            <a href="{{ route('home') }}" class="hover:text-yellow-500">Home</a>
-            <a href="{{ route('about') }}" class="hover:text-yellow-500">About</a>
-            <a href="{{ route('gallery') }}" class="hover:text-yellow-500">Gallery</a>
-            <a href="{{ route('portfolio') }}" class="hover:text-yellow-500">Portfolio</a>
-            <a href="{{ route('contact') }}" class="hover:text-yellow-500">Contact</a>
-            <div class="flex space-x-4 mt-4">
-                <a href="https://facebook.com" target="_blank" class="hover:text-blue-600">
+        <div id="mobile-menu" class="hidden md:hidden flex flex-col space-y-4 p-4 text-md font-semibold">
+            <a href="{{ route('home') }}" class="text-yellow-500 hover:text-white"> <i class="bi bi-house-fill"></i> Home</a>
+            <a href="{{ route('about') }}" class="text-yellow-500 hover:text-white"> <i class="bi bi-person-circle"></i> About</a>
+            <a href="{{ route('gallery') }}" class="text-yellow-500 hover:text-white"><i class="bi bi-images"></i> Gallery</a>
+            <a href="{{ route('portfolio') }}" class="text-yellow-500 hover:text-white"> <i class="bi bi-images"></i> Portfolio</a>
+            <a href="{{ route('contact') }}" class="text-yellow-500 hover:text-white"><i class="bi bi-phone-vibrate-fill"></i> Contact</a>
+            <a href="{{ route('budget.index') }}" class="text-yellow-500 hover:text-white"><i class="bi bi-cash-coin"></i> Budget</a>
+            
+            <div class="flex flex-col space-y-4 space-x-4 mt-4">
+                
+
+                <a href="{{ route('category.view') }}"
+                class=" w-48 py-2 ml-4 block text-center text-yellow-400 border border-yellow-400 rounded hover:bg-yellow-400 hover:text-white transition">
+                Book Now
+                </a>
+
+                @auth
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit"
+                        class="w-48 py-2 mr-0 text-yellow-400 border border-yellow-400 rounded hover:bg-yellow-400 hover:text-white transition">
+                        Logout
+                    </button>
+                </form>
+                @endauth
+                @guest
+                <form action="{{ route('login') }}">
+                    @csrf
+                    <button type="submit"
+                        class="w-48 py-2 mr-0 text-yellow-400 border border-yellow-400 rounded hover:bg-yellow-400 hover:text-white transition">
+                        Login
+                    </button>
+                </form>
+                @endguest
+                {{-- <a href="https://facebook.com" target="_blank" class="hover:text-blue-600">
                     <i class="bi bi-facebook text-xl"></i>
                 </a>
                 <a href="https://instagram.com" target="_blank" class="hover:text-pink-500">
@@ -73,7 +125,7 @@
                 </a>
                 <a href="https://twitter.com" target="_blank" class="hover:text-blue-400">
                     <i class="bi bi-twitter text-xl"></i>
-                </a>
+                </a> --}}
             </div>
         </div>
         
